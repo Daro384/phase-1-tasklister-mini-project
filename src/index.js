5742c631953544f92d843e77a1f4a9329d3b82cd
Joseph <joffan98@gmail.com>
2022-09-21 18:25:07 -0500

    first Draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //Functionallity for "create new Text" Button
+  const newTask = (event) => {
+    event.preventDefault()
+
+    //create toDoItem
+    const newToDo = document.createElement("li")
+    newToDo.textContent = document.getElementById("new-task-description").value
+    
+    //add remove Button
+    const removeButton = document.createElement("button")
+    removeButton.textContent = "X"
+    removeButton.addEventListener("click", () => {
+      newToDo.remove()
+    })
+    newToDo.append(removeButton)
+
+    const toDoList = document.getElementById("tasks")
+    toDoList.append(newToDo)
+    
+  }
+  
+  const taskSubmit = document.getElementById("create-task-form")
+  taskSubmit.addEventListener("submit", newTask)
+
 });
